7baf2813db2fb8435dbb37223a08034207c8c7a9
Jedd <jeddrmorgan@gmail.com>
2019-05-01 00:53:39 +0100

    Merged robots and tiles

--- src/robotGame/GameManager.java
+++ src/robotGame/GameManager.java
@@ -17,7 +17,7 @@ import utils.Point;
 /**
  * GameManager
  * Instantiates board, and controls rounds
- *
+ * 
  * @author Jedd Morgan
  * @author Owen Craig
  * @version 29/04/2019
@@ -31,20 +31,31 @@ public class GameManager extends GameObject implements IUpdatable {
 	/** Time in seconds between each Turn*/
 	private static final double TURN_TIME = 1;
 
+	private static final int roundLength = 5;
+	private int turnNumber = 0;
+
 	private Queue<Robot> robots;
 
-	private HashMap<Integer, ArrayList<Instruction[]>> players;
+	private HashMap<Integer, LinkedList<Instruction[]>> players;
 	private ArrayList<Point> startingLocations;
+	
+	private String programFile;
 
-	public GameManager()
+	public GameManager(String programFile) 
 	{
-		players = new HashMap<Integer, ArrayList<Instruction[]>>();
+		players = new HashMap<Integer, LinkedList<Instruction[]>>();
 		robots = new LinkedList<Robot>();
+		this.tag = "gameManager";
+		this.programFile = programFile;
+	}
+	
+	public GameManager() 
+	{
+		this("2players-2rounds");
 	}
 
-
 	@Override
-	public void init()
+	public void init() 
 	{
 		//creating a new board
 		board = new Board();
@@ -67,9 +78,9 @@ public class GameManager extends GameObject implements IUpdatable {
 	}
 
 
-	private HashMap<Integer, ArrayList<Instruction[]>> formatInstructions(String[] text)
+	private HashMap<Integer, LinkedList<Instruction[]>> formatInstructions(String[] text)
 	{
-		//checks for a format line
+		//checks for a format line 
 		if (!text[0].startsWith("format "))
 		{
 			Logger.log(this, LogSeverity.ERROR, "Could not discern format from instruction data.");
@@ -84,17 +95,16 @@ public class GameManager extends GameObject implements IUpdatable {
 		{
 		case 1:
 			return loadInstructionsFormat1(text);
-		//default error for an invalid formatVersion
+			//default error for an invalid formatVersion
 		default:
 			Logger.log(this, LogSeverity.ERROR, "Instruction data had invalid version: {" + formatVersion + "}");
 		}
 		return null;
 	}
 
-	private HashMap<Integer, ArrayList<Instruction[]>> loadInstructionsFormat1(String[] text )
-	{
+	private HashMap<Integer, LinkedList<Instruction[]>> loadInstructionsFormat1(String[] text )
+	{	
 		final int INSTRUCTION_LINE_STARTS = 2; //Instructions start on line (starting from 0)
-		final int EXPECTED_NUM_INSTRUCTIONS = 5; //Expected number of instructions for each player per round
 
 		//VALIDATE instruction file contains instructions
 		if (text.length < INSTRUCTION_LINE_STARTS + 1)
@@ -103,7 +113,7 @@ public class GameManager extends GameObject implements IUpdatable {
 			return null;
 		}
 
-		HashMap<Integer, ArrayList<Instruction[]>> players = new HashMap<Integer, ArrayList<Instruction[]>>();
+		HashMap<Integer, LinkedList<Instruction[]>> players = new HashMap<Integer, LinkedList<Instruction[]>>();
 
 		//For each round
 		for (int y = INSTRUCTION_LINE_STARTS; y < text.length; y++)
@@ -120,7 +130,7 @@ public class GameManager extends GameObject implements IUpdatable {
 				Instruction[] playerInstructions = new Instruction[roundInstructions[playerNum].length()];
 
 				//VALIDATE playerRound is of expected size
-				if (roundInstructions[playerNum].length() != EXPECTED_NUM_INSTRUCTIONS)
+				if (roundInstructions[playerNum].length() != roundLength)
 				{
 					Logger.log(this, LogSeverity.ERROR, "Instruction file contains inconsistant number of instructions");
 					return null;
@@ -152,7 +162,7 @@ public class GameManager extends GameObject implements IUpdatable {
 					case 'W':
 						playerInstructions[j] = Instruction.WAIT;
 						break;
-					//default error for invalid instruction values
+						//default error for invalid instruction values
 					default:
 						Logger.log(this, LogSeverity.ERROR, "Encountered an invalid character while reading program file: {" + c + "}");
 						break;
@@ -160,11 +170,10 @@ public class GameManager extends GameObject implements IUpdatable {
 				}
 
 				//updates the instruction arrays of each player
-				ArrayList<Instruction[]> temp = players.get(playerNum);
-				if (temp == null) temp = new ArrayList<Instruction[]>();
+				LinkedList<Instruction[]> temp = players.get(playerNum);
+				if (temp == null) temp = new LinkedList<Instruction[]>();
 				temp.add(playerInstructions);
 				players.put(playerNum, temp);
-
 			}
 
 		}
@@ -177,57 +186,73 @@ public class GameManager extends GameObject implements IUpdatable {
 	 * What needs to be done is fairly simple but we can't really progress with this turn thing without
 	 * V's input system which is very close to completion ( or depending on V's ability to power work through the night is complete but would be on the master branch )
 	 * I suggest you wait for the completion of said input system, and me to merge the two branches so that we can continue
-	 *
+	 * 
 	 * When that has happened, We basically want to not start running instructions until a start button is pressed
 	 * The way we check for that is unclear at the moment and requires V to decide how button events will be handled
 	 * Once the round has started we execute robot instructions with the delay of TURN_TIME
-	 *
-	 * Then we wait until the round start button is pressed again.
+	 * 
+	 * Then we wait until the round start button is pressed again. 
 	 * This will allow for future expandability when we use the input system to its entirety to get user input through the keyboard listener
 	 * Rather than through the program file.
 	 * Very nice ;)
 	 */
 	@Override
-	public void update(double time) {
+	public void update(double time) 
+	{
 		rDeltaT += time;
 
-		if (rDeltaT > TURN_TIME) {
+		if(rDeltaT > TURN_TIME)
+		{
+			if(turnNumber != 0 && turnNumber <= roundLength)
+			{
+				turn();
+			}
 			rDeltaT = 0;
-
-			round();
 		}
 	}
 
+	@Override
+	public void destroy() 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	
 	/**
 	 * Executes a round, round is made up of turns one for each player and one for the board
 	 */
-	
-	private void round() {
-		for(int i = 0; i < robots.size(); i++) {
-			//Each Player Turn
+	public void setRound() 
+	{
+		for (int i=0;i < robots.size();i++)
+		{
+			Robot r = robots.poll();
+			r.setInstructions(players.get(i).poll());
+			robots.offer(r);
+		}
+		turnNumber = 1;
+	}
+
+	private void turn()
+	{
+		for(int i = 0; i < robots.size(); i++)
+		{
 			Robot r = robots.poll();
 			r.act();
-
-			robots.add(r);
+			robots.offer(r);
 		}
-		robots.add(robots.poll());
 
-		//
-		for(BoardTile tile : board.getBoardTiles()) {
-			tile.act();
+		for(int i = 0; i < robots.size(); i++) 
+		{
+			//Each Player Turn
+			Robot r = robots.poll();
+			Board.getTile(r.getIndex()).act();
+
+			robots.offer(r);
 		}
-		Logger.log(this, LogSeverity.INFO, "Done a turn");
+		turnNumber++;
 	}
 
 
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
-
-
-
-
