6a92eec66cb4017ec9fe636b0b38fdb51f93abd9
Jedd <jeddrmorgan@gmail.com>
2019-05-01 15:53:57 +0100

    Merged tiles and JUnit

---
+++ src/junit/BoardTest.java
@@ -0,0 +1,24 @@
+package junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import robotGame.Board;
+
+class BoardTest {
+
+	private Board board;
+
+	@Before
+	public void boardTest() {
+		board = new Board();
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
